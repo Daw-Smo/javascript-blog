a471afcb22469fbe88fc4adfaa09bbb12733418d
Dawid Smolarek <info@butikloox.pl>
2023-02-04 21:51:23 +0100

    Error in js line 84

--- js/script.js
+++ js/script.js
@@ -46,3 +46,51 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+    /* remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for (let article of articles) {
+
+        /* get the article id */
+
+        const articleID = article.getAttribute('id');
+        console.log(articleID)
+
+        /* find the title element */
+
+        const titleElement = article.querySelector(optTitleSelector);
+
+        /* get the title from the title element */
+
+        const articleTitle = titleElement.innerHTML;
+
+        /* create HTML of the link */
+
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML)
+        /* insert link into titleList */
+
+        html = html + linkHTML;
+
+    }
+    
+    titleList.innerHTML = html;
+    console.log(html)
+}
+
+generateTitleLinks();
